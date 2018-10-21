1444780311e89fd65831d9da6ed346dc67540a35
cai4633 <463353522@qq.com>
2018-10-21 14:31:11 +0800

    v1.0.1    测试git功能；

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export default {
 	navi:[
 		{index:'demo1',title:'星级评分系统'},
-		{index:'demo2',title:'星级评分系统'},
-		{index:'demo3',title:'星级评分系统'}
+		{index:'demo2',title:'在线计算器'},
+		{index:'demo3',title:''}
 	]
 };
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home.vue';
 import Demo1 from '@/components/Demo1';
+import Demo2 from '@/components/Demo2';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
@@ -19,6 +20,11 @@ export default new Router({
 			path:'/demo1',
 			name:'demo1',
 			component:Demo1
+		},
+		{
+			path:'/demo2',
+			name:'demo2',
+			component:Demo2
 		}
 	]
 });
